3b1f31e10f81cf601f6aefc34834da68eb41ca20
Amila Ruwan Guruge <amilaruwan9349@gmail.com>
2022-06-15 00:11:18 +0200

    Perfect

--- FinneJobberWeb/Areas/JobSeeker/Controllers/HomeController.cs
+++ FinneJobberWeb/Areas/JobSeeker/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using FinneJobber.Models;
+﻿using FinneJobber.DataAccess.Repository.IRepository;
+using FinneJobber.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace FinneJobberWeb.Areas.JobSeeker.Controllers;
@@ -8,13 +10,16 @@ namespace FinneJobberWeb.Areas.JobSeeker.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IUnitOfWork _unitOfWork;
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
     public IActionResult Index()
         {
-            return View();
+            IEnumerable<Job> jobList = _unitOfWork.Job.GetAll(includeProperties:"Category");
+            return View(jobList);
         }
     public IActionResult Privacy()
         {
