a98b222cb5c3a15071779ee7d16a66dfe7dad18e
Amila Ruwan Guruge <amilaruwan9349@gmail.com>
2022-06-15 20:46:04 +0200

    Trying to apply

--- FinneJobberWeb/Areas/JobSeeker/Controllers/HomeController.cs
+++ FinneJobberWeb/Areas/JobSeeker/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using FinneJobber.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace FinneJobberWeb.Areas.JobSeeker.Controllers;
 [Area("JobSeeker")]
@@ -30,4 +31,40 @@ public class HomeController : Controller
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    #region API CALLS
+    //Post
+    [HttpPost]
+    public IActionResult Apply(int? id)
+    {
+        var obj = _unitOfWork.Job.GetFirstOrDefault(u => u.Id == id);
+        if (obj == null)
+        {
+            return Json(new { success = false, message = "Error while applying" });
+        }
+
+        var claimsIdentity = User.Identity as ClaimsIdentity;
+        if (claimsIdentity != null)
+        {
+            // the principal identity is a claims identity.
+            // now we need to find the NameIdentifier claim
+            var userIdClaim = claimsIdentity.Claims
+                .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            if (userIdClaim != null)
+            {
+                var userIdValue = userIdClaim.Value;
+                _unitOfWork.Job.Update(obj);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "Delete successful" });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Error while applying" });
+            }
+        }
+        else {
+            return Json(new { success = false, message = "Please Loggin" });
+        }
+    }
+    #endregion
 }
